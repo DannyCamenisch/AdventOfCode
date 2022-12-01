d0d7dcd35726092960909732b55bd41cbd824f50
Danny Camenisch <danny@camenisch.ws>
2022-12-01 07:12:17 +0100

    ⭐ finished day01

--- 2022/solutions/day01.py
+++ 2022/solutions/day01.py
@@ -5,21 +5,13 @@ from utils import advent
 advent.setup(2022, 1)
 fin = advent.get_input()
 
-numbers = tuple(map(int, fin.readlines()))
-string  = tuple(fin.readlines())
+blocks = [[int(line) for line in block.split('\n')] for block in fin.read()[:-1].split('\n\n')]
+blocks = [sum(block) for block in blocks]
+
+sorted_blocks = sorted(blocks, reverse=True)
 
 # Part 1 
-ans = 0
-for i in range(len(numbers) - 1):
-    if numbers[i] < numbers[i+1]:
-        ans += 1
-
-advent.submit_answer(1, ans)
+advent.submit_answer(1, sorted_blocks[0])
 
 # Part 2
-ans = 0
-for i in range(len(numbers) - 1):
-    if numbers[i] < numbers[i+3]:
-        ans += 1
-
-advent.submit_answer(2, ans)
+advent.submit_answer(2, sum(sorted_blocks[0:3]))
