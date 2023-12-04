292dbd306e1192a5b968e17df5bba0c3a76c2038
dcamenisch <danny@camenisch.ws>
2023-12-04 18:57:00 +0100

    🎄 add placeholder for remaining days

---
+++ 2023/solutions/day11.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from utils import advent
+
+advent.setup(2023, 11)
+fin = advent.get_input()
+
+# Part 1 
+# advent.submit_answer(1, _)
+
+# Part 2
+# advent.submit_answer(2, _)
