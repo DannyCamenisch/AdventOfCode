3fb763f29fc7795846a180cc38b4d32adb8e8a2b
dcamenisch <danny@camenisch.ws>
2023-12-09 13:56:37 +0100

    🎄 solved day09

--- 2023/solutions/day09.py
+++ 2023/solutions/day09.py
@@ -5,8 +5,32 @@ from utils import advent
 advent.setup(2023, 9)
 fin = advent.get_input()
 
-# Part 1 
-# advent.submit_answer(1, _)
+lines = [[int(n) for n in line.split()] for line in fin.readlines()]
+
+def find_previous(sequence):
+    new_sequence = []
+    for i in range(len(sequence) - 1):
+        new_sequence.append(sequence[i + 1] - sequence[i])
+        
+    if all(n == 0 for n in new_sequence):
+        return sequence[0] - 0
+    else:
+        return sequence[0] - find_previous(new_sequence)
+    
+def find_next(sequence):
+    new_sequence = []
+    for i in range(len(sequence) - 1):
+        new_sequence.append(sequence[i + 1] - sequence[i])
+        
+    if all(n == 0 for n in new_sequence):
+        return sequence[-1] + 0
+    else:
+        return sequence[-1] + find_next(new_sequence)
+
+# Part 1
+result_1 = sum(find_next(line) for line in lines)
+advent.submit_answer(1, result_1)
 
 # Part 2
-# advent.submit_answer(2, _)
+result_2 = sum(find_previous(line) for line in lines)
+advent.submit_answer(2, result_2)
