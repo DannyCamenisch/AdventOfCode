5b5e5376f9c94db9011b79a6d9a33f2c7e85f840
dcamenisch <danny@camenisch.ws>
2023-12-25 12:24:35 +0100

    🎄 solved day25

--- 2023/solutions/day25.py
+++ 2023/solutions/day25.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
+import networkx as nx
+
 from utils import advent
 
 advent.setup(2023, 25)
 fin = advent.get_input()
 
-# Part 1 
-# advent.submit_answer(1, _)
+graph = nx.Graph()
+for l in fin.read().splitlines():
+    a, *dsts = l.split()
+    a = a[:-1]
+    for dst in dsts:
+        graph.add_edge(a, dst)
 
-# Part 2
-# advent.submit_answer(2, _)
+_, partitions = nx.stoer_wagner(graph)
+
+# Part 1
+result_1 = len(partitions[0]) * len(partitions[1])
+advent.submit_answer(1, result_1)
